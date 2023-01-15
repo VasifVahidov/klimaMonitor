226e5559fa712c712ffb1ef51adb03e633103e8d
VasifVahidov <vasif202@gmail.com>
2023-01-15 17:21:20 +0100

    Creating a first spring boot (born)

--- src/main/java/com/example/klimaMonitor/KlimaMonitorApplication.java
+++ src/main/java/com/example/klimaMonitor/KlimaMonitorApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KlimaMonitorApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(KlimaMonitorApplication.class, args);
+		System.out.println("Hello Github!");
 	}
 
 }
