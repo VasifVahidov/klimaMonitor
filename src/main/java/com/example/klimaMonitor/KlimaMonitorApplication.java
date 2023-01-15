ceea282993c9ac62cea9730f6e561efb3aae11c3
VasifVahidov <vasif202@gmail.com>
2023-01-15 17:15:41 +0100

    Creating a first spring boot (born)

---
+++ src/main/java/com/example/klimaMonitor/KlimaMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.klimaMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlimaMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlimaMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/klimaMonitor/KlimaMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.klimaMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlimaMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
